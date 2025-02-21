3edd20181c11110fc1f0d86438c03cc8a3846fe0
Roi <r.lopezbarata@gmail.com>
2025-02-21 10:57:02 +0100

    Logout e inicio de sesión avanzado.
    - Creación e implementación completa de "logout.php", que se encarga de todo el proceso de cerrar la sesión.
    - Modificación del menú, para que muestre distintos links según haya iniciado o no la sesión el usuario.
    - Implementados los redireccionamientos necesarios desde login y desde logout.

--- Entrega2/menu.php
+++ Entrega2/menu.php
@@ -4,7 +4,23 @@
 <nav align="center" >
 	<span><a href="index.php">Care4Pet</a></span> &emsp;&emsp;
 	<span><a href="pagina_contratacion.php">Contratar cuidadores</a></span> &emsp;&emsp;
-	<span><a href="mis_reservas.php">Mis reservas</a></span> &emsp;&emsp;
-	<span><a href="registro.php">Registrarse</a></span> &emsp;&emsp;
-	<span><a href="inicio_sesion.php">Iniciar sesión</a></span> &emsp;&emsp;
+	<span><a href="sobre_nosotros.php">Sobre nosotros</a></span> &emsp;&emsp;
+	
+	<?php
+		// Si el usuario ha iniciado sesión, le aparecerá el link a sus reservas y el 
+		// botón de cerrar sesión.
+		if (isset($_SESSION["login"]) && $_SESSION["login"] == true) {
+			echo "<span><a href=\"mis_reservas.php\">Mis reservas</a></span> &emsp;&emsp;";
+			echo "<span><a href=\"logout.php\">Cerrar sesión</a></span> &emsp;&emsp;";
+		}
+		
+		// Si el usuario no ha iniciado sesión, le aparecerán los enlaces a las páginas
+		// de registro e inicio de sesión.
+		else {
+			echo "<span><a href=\"registro.php\">Registrarse</a></span> &emsp;&emsp;";
+			echo "<span><a href=\"inicio_sesion.php\">Iniciar sesión</a></span> &emsp;&emsp;";
+
+		}
+	?>
+	
 </nav>
--- Entrega2/procesar_login.php
+++ Entrega2/procesar_login.php
@@ -17,14 +17,24 @@
         "SELECT * FROM usuarios 
         WHERE Correo = '$email' and Contraseña = '$contrasena'";
 
-        $resultado = $con->query($sentencia_sql);
+        $consulta = $con->query($sentencia_sql);
 
         // Si hay un email con esa contraseña asociada, las credenciales son
         // válidas y se inicia sesión.
-        if ($resultado->num_rows > 0) {
+        if ($consulta->num_rows > 0) {
             echo "Sesión iniciada con éxito.<br>";
-            $_SESSION["email"] = $email;
+
+            // Incluye la fila de valores resultante de la sentencia en un array.
+            $filaResultado = $consulta->fetch_assoc();
+
+            // Crea algunas variables de sesión con los datos principales del 
+            // usuario obtenidos con la consulta a la base de datos.
             $_SESSION["login"] = true;
+            $_SESSION["email"] = $filaResultado["Correo"];
+            $_SESSION["nombreUsuario"] = $filaResultado["Nombre"];
+
+            // Redirige al usuario a la página de inicio.
+            header("Location: index.php");
         } 
         
         // Si el email o la contraseña son incorrectos, no se inicia sesión.
@@ -32,7 +42,8 @@
             echo "Correo o contraseña incorrectos. Por favor, inténtelo de nuevo.<br>";
         }
 
-        // Se cierra la conexión con la BD (puede que esta línea haya que omitirla...)
+
+        // Se cierra la conexión con la BD (puede que esta línea haya que omitirla...).
         $con->close();
         
     }
