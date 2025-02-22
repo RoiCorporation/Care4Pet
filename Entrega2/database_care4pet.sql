5109457c53cc1d3714513adf9412076e000502c3
Roi <r.lopezbarata@gmail.com>
2025-02-22 13:40:34 +0100

    Completadas todas las tablas SQL

--- Entrega2/database_care4pet.sql
+++ Entrega2/database_care4pet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-02-2025 a las 13:03:46
+-- Tiempo de generación: 22-02-2025 a las 13:40:14
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -24,12 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cuidadores`
+--
+
+CREATE TABLE `cuidadores` (
+  `idUsuario` bigint(20) UNSIGNED NOT NULL,
+  `TiposDeMascotas` longtext DEFAULT NULL,
+  `Tarifa` decimal(10,0) UNSIGNED NOT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `ServiciosAdicionales` longtext DEFAULT NULL,
+  `Valoracion` tinyint(3) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `duenos`
 --
 
 CREATE TABLE `duenos` (
   `idUsuario` bigint(20) UNSIGNED NOT NULL,
   `idMascota` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `idMascota` bigint(20) UNSIGNED NOT NULL,
+  `FotoMascota` text DEFAULT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `TipoMascota` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `idReserva` bigint(20) UNSIGNED NOT NULL,
+  `idMascota` bigint(20) UNSIGNED NOT NULL,
+  `idCuidador` bigint(20) UNSIGNED NOT NULL,
+  `FechaInicio` datetime NOT NULL,
+  `FechaFin` datetime NOT NULL,
+  `esAceptadaPorCuidador` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
+  `Valoracion` tinyint(3) UNSIGNED DEFAULT NULL,
+  `Resena` text DEFAULT NULL,
+  `ComentariosAdicionales` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios_adicionales`
+--
+
+CREATE TABLE `servicios_adicionales` (
+  `idServicio` bigint(20) UNSIGNED NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Coste` decimal(10,0) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_de_mascotas`
+--
+
+CREATE TABLE `tipos_de_mascotas` (
+  `idTipoMascota` bigint(20) UNSIGNED NOT NULL,
+  `Nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -48,10 +117,10 @@ CREATE TABLE `usuarios` (
   `Telefono` int(11) NOT NULL,
   `FotoPerfil` text DEFAULT NULL,
   `Direccion` varchar(255) DEFAULT NULL,
-  `esDueno` tinyint(1) NOT NULL,
-  `esCuidador` tinyint(1) NOT NULL,
-  `esAdmin` tinyint(1) NOT NULL,
-  `cuentaActiva` tinyint(1) NOT NULL
+  `esDueno` tinyint(1) NOT NULL DEFAULT 0,
+  `esCuidador` tinyint(1) NOT NULL DEFAULT 0,
+  `esAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  `cuentaActiva` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -67,10 +136,40 @@ INSERT INTO `usuarios` (`idUsuario`, `Nombre`, `Apellidos`, `Correo`, `Contrase�
 --
 
 --
+-- Indices de la tabla `cuidadores`
+--
+ALTER TABLE `cuidadores`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
 -- Indices de la tabla `duenos`
 --
 ALTER TABLE `duenos`
   ADD PRIMARY KEY (`idUsuario`,`idMascota`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`idMascota`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`idReserva`);
+
+--
+-- Indices de la tabla `servicios_adicionales`
+--
+ALTER TABLE `servicios_adicionales`
+  ADD PRIMARY KEY (`idServicio`);
+
+--
+-- Indices de la tabla `tipos_de_mascotas`
+--
+ALTER TABLE `tipos_de_mascotas`
+  ADD PRIMARY KEY (`idTipoMascota`);
 
 --
 -- Indices de la tabla `usuarios`
