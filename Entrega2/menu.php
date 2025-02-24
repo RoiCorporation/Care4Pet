401227f84549a9ba3e52c348bff387a802741586
Roi <r.lopezbarata@gmail.com>
2025-02-24 14:29:24 +0100

    Actualizado menu.php

--- Entrega2/menu.php
+++ Entrega2/menu.php
@@ -1,6 +1,4 @@
 <!-- Menú principal de la aplicación -->
-
-<!-- Reemplazar el atributo "align" cuanto antes, está obsoleto align="center"-->
 <nav style="text-align:center">
 	<span><a href="index.php">Care4Pet</a></span> &emsp;&emsp;
 	<span><a href="pagina_contratacion.php">Contratar cuidadores</a></span> &emsp;&emsp;
