4c6162c512113bc80c2b02cb1bcbd1a34195b3a4
ccecconi01 <ccecconi@ucm.es>
2025-02-27 23:31:17 +0000

    perfil_cuidador primeras alteracciones

--- Entrega2/perfil_cuidador.php
+++ Entrega2/perfil_cuidador.php
@@ -1,46 +1,79 @@
 <?php
-	session_start();
-	require 'database.php';
+    session_start();
+    
+    require_once 'DatabaseConnection.php';
+    require 'DAOUsuario.php';
+    require 'DAOCuidador.php';
+
+    $usuario = NULL;
+    $cuidador = NULL;
+
+    if (isset($_SESSION["login"]) && $_SESSION["login"] == true) {
+        // obtenemos informacion basica sobre el dueno de BD
+        $id = $_SESSION["id"];
+
+        // Obtenemos info del usario mediante el DAO
+        $usuario = (DAOUsuario::getInstance())->leerUnUsuario($_SESSION["email"]);
+
+        // consulatmos la BD para obtener las infos del cuidador
+        $cuidador = (DAOCuidador::getInstance())->leerUnCuidador($id);
+    }
 ?>
-
-//tabla cuidadores 
-
-<!-- CREATE TABLE `cuidadores` (
-  `idUsuario` bigint(20) UNSIGNED NOT NULL,
-  `TiposDeMascotas` longtext DEFAULT NULL,
-  `Tarifa` decimal(10,0) UNSIGNED NOT NULL,
-  `Descripcion` text DEFAULT NULL,
-  `ServiciosAdicionales` longtext DEFAULT NULL,
-  `Valoracion` tinyint(3) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
- -->
 
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Perfil del cuidador</title>
-	</head>
+    <head>
+        <link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Perfil del cuidador</title>
+    </head>
 
-
-	<body>
+    <body>
 
     <?php
-		require 'cabecera.php';
-	?>
+        require 'cabecera.php';
+    ?>
 
-	<!-- Contenido principal de la página de mis reservas -->
-	<div style="padding-left: 200px; padding-right: 200px">
+    <!-- Contenido principal de la página de mis reservas -->
+    <div style="padding-left: 200px; padding-right: 200px">
     
         <h2 style="text-align:center">Mi perfil</h2>
+        <main class="perfil-container">
+        <section class="perfil-info">
+            <h2>Información personal</h2>
+            <?php if ($usuario): ?> //verifica si no es null
+                <p>Nombre: <?= htmlspecialchars($usuario->nombre); ?></p>
+                <p>Email: <?= htmlspecialchars($usuario->correo); ?></p>
+                <p>Telefono: <?= htmlspecialchars($usuario->telefono); ?></p>
+                <p>Localidad: <?= htmlspecialchars($usuario->direccion); ?></p>
+                <div class="foto-perfil">
+                    <img src="<?= htmlspecialchars($usuario->fotoPerfil ?? 'default.png'); ?>" alt="Foto de perfil">
+                </div>
+                <h2>Hola, <?= htmlspecialchars($usuario->nombre); ?>! Bienvenidos a tu perfil!</h2>
+            <?php else: ?>
+                <p>No se pudo cargar la información del usuario.</p>
+            <?php endif; ?>
+		</section>
+            <section div class="detalles-servicios">
+                <h2>Detalles de sus servicios</h2>
+                <?php if ($cuidador): ?> //verifica si no es null 
+					<p>Valoración: <?= htmlspecialchars($cuidador->valoracion); ?></p>
+					<p>Zonas atendidas: <?= htmlspecialchars($cuidador->zonasAtendidas); ?></p>
+					<p>Tarifa: <?= htmlspecialchars($cuidador->tarifa); ?> €/día</p>
+					<p>Tipos de mascotas aceptadas: <?= htmlspecialchars($cuidador->tiposDeMascotas); ?></p>
+                    <p>Descripción al Cliente: <?= htmlspecialchars($cuidador->descripcion); ?></p>
+                    <p>Servicios Adicionales: <?= htmlspecialchars($cuidador->serviciosAdicionales); ?></p>
+					
+                <?php else: ?>
+                    <p>No se pudo cargar la información del cuidador.</p>
+                <?php endif; ?>
+            </section>
+        </section>
+        </main>
+    </div>
 
-		<p>Contenido...</p>
-	</div>
+    <?php require 'pie_pagina.php'?>
+    <?php require 'aviso_legal.php'?>
 
-	<?php require 'pie_pagina.php'?>
-	<?php require 'aviso_legal.php'?>
-
-	</body>
-
+    </body>
 </html>
