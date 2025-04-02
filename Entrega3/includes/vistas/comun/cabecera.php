cde6300dc3fcebffec9791fbbfdaad9c7a1ac493
Clarisse Bueno Netto Cecconi <ccecconi@ucm.es>
2025-04-02 23:04:08 +0200

    menu logo fix

--- Entrega3/includes/vistas/comun/cabecera.php
+++ Entrega3/includes/vistas/comun/cabecera.php
@@ -1,8 +1,8 @@
 <!-- Cabecera a incluir en todas las páginas -->
 <header>
-<div class="logo-container">
+
     <img src="img/logo_menor.png" alt="Logo Menor" />
-  </div>
+  
     <h1 style="text-align:center">Care4Pet</h1>
     <h3 style="text-align:center">Cuidamos de tu mascota</h3>
     <?php
