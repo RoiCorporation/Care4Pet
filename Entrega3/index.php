cab0b16d08fc747fa40edf4c54efc510069f7f58
Clarisse Bueno Netto Cecconi <ccecconi@ucm.es>
2025-04-02 21:19:12 +0200

    header con imagen

--- Entrega3/index.php
+++ Entrega3/index.php
@@ -11,22 +11,15 @@
 		<div class="contenedor-general">
 
 			<p>
-				Care4Pet es una aplicación web diseñada para conectar a dueños de mascotas con cuidadores de confianza, 
-				ofreciendo servicios personalizados para garantizar el bienestar de los animales. A través de esta plataforma, 
-				los dueños pueden buscar cuidadores según su disponibilidad y experiencia, contratar servicios como alojamiento 
-				temporal, paseos, cuidados a domicilio o visitas, y gestionar fácilmente las fechas, tarifas y requisitos 
-				específicos. Los cuidadores, por su parte, pueden registrar su disponibilidad y especificar qué tipos de 
-				mascotas pueden atender.
+				Care4Pet es una aplicación web diseñada para conectar a dueños de mascotas con cuidadores de confianza, ofreciendo servicios personalizados para garantizar el bienestar de los animales. A través de esta plataforma, los dueños pueden buscar cuidadores según su disponibilidad y experiencia, contratar servicios como alojamiento temporal, paseos, cuidados a domicilio o visitas, y gestionar fácilmente las fechas, tarifas y requisitos específicos. Los cuidadores, por su parte, pueden registrar su disponibilidad y especificar qué tipos de mascotas pueden atender.
 			</p>
 
 			<p>
-				La app web incluye un sistema de valoraciones y reseñas para asegurar la calidad de los servicios y la confianza 
-				entre los usuarios.
+				La app web incluye un sistema de valoraciones y reseñas para asegurar la calidad de los servicios y la confianza entre los usuarios.
 			</p>
 
 			<p>
-				Care4Pet simplifica el proceso de cuidado animal con una experiencia accesible, segura y eficiente desde cualquier 
-				dispositivo con conexión a internet.<br><br>
+				Care4Pet simplifica el proceso de cuidado animal con una experiencia accesible, segura y eficiente desde cualquier dispositivo con conexión a internet.<br><br>
 			</p>
 
 			<img src="img/LogoCare4PetTransparente.png" alt="Logo de Care4Pet" class="logo">
