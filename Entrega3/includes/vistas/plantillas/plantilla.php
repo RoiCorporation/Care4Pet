02e8fdf0ac634e67d1152a19d157bdd2cbcce9cb
Roi <r.lopezbarata@gmail.com>
2025-04-03 11:47:03 +0200

    Corregido el enlace al estilo.css en la plantilla.

--- Entrega3/includes/vistas/plantillas/plantilla.php
+++ Entrega3/includes/vistas/plantillas/plantilla.php
@@ -6,7 +6,7 @@
         <title><?= $tituloPagina ?></title>
         <!--<link rel="stylesheet" type="text/css" href="/Entrega3/CSS/estilo.css" />-->
         <style>
-            <?php readfile(__DIR__ . '/../../../Css/estilo.css'); ?>
+            <?php readfile(__DIR__ . '/../../../CSS/estilo.css'); ?>
         </style>
 
     </head>
