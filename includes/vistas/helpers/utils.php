c8d8017e63ed5e36d9802536096665e9f7d00912
Roi <r.lopezbarata@gmail.com>
2025-05-06 17:13:10 +0200

    Incluidos en los mensajes la fecha y hora de su envío.
    - Modificado estilos.css para que almacene las clases relativas a la fecha y hora de los mensajes. Actualizada la clase scroller-mensajes, así como mensaje-propio y mensaje-otro, para que adapten su tamaño a los límites de la ventana.
    - Actualizado utils.php para que incluya los atributos horarios en cada mensaje del chat.

--- includes/vistas/helpers/utils.php
+++ includes/vistas/helpers/utils.php
@@ -19,9 +19,15 @@
         // Inserta cada mensaje en la variable de contenido.
         foreach($mensajesConversacion as $mensaje) {
 
+            // Invoca a la función que obtiene el párrafo con la hora y la
+            // fecha formateado y listo para incluir en el HTML.
+            $parrafoHoraYFecha = obtenerFechaMensajeFormateada($mensaje);
+
             // Si el emisor del mensaje es el propio usuario, asocia a ese 
             // mensaje la clase de estilos "mensaje-propio".
             if ($mensaje->getIdUsuarioEmisor() == $_SESSION['id']) {
+
+                // Añade al mensaje la hora y fecha del mismo.
                 $contenidoSeccionMensajes .=
                     '<div class="mensaje-propio">';
 
@@ -43,19 +49,25 @@
                 // Añade el formulario de borrado al mensaje.
                 $contenidoSeccionMensajes .= $formularioBorrarMensaje->gestiona();
 
-                // Añade al div del mensaje el texto del mismo.
-                $contenidoSeccionMensajes .= 
+                // Añade al mensaje el texto del mismo, así como el párrafo
+                // de hora y fecha en la que fue enviado.
+                $contenidoSeccionMensajes .=
                         '<p>' . $mensaje->getMensaje() . '</p>
-                    </div><br>';               
+                    </div>' . $parrafoHoraYFecha . 
+                    '<br>';
             }
 
             // Si el emisor del mensaje es el otro usuario, asocia a ese 
             // mensaje la clase de estilos "mensaje-otro-usuario".
             else {
+
+                // Añade al mensaje el texto del mismo, así como el párrafo
+                // de hora y fecha en la que fue enviado.
                 $contenidoSeccionMensajes .=
                     '<div class="mensaje-otro-usuario">
                         <p>' . $mensaje->getMensaje() . '</p>
-                    </div><br>';               
+                    </div>' . $parrafoHoraYFecha . 
+                    '<br>';
             }
         }
 
@@ -76,6 +88,49 @@
 
     }
 
+
+
+    // Función auxiliar que, dado un mensaje, devuelve un párrafo HTML (<p>)
+    // formateado con la información relevante de su fecha. Se utiliza al
+    // incluir dichos datos temporales en los mensajes que se muestran en
+    // cada chat.
+    function obtenerFechaMensajeFormateada($mensaje) {
+
+        // Variable en la que se guardará la clase de estilo del párrafo
+        // de hora y fecha.
+        $claseParrafoHoraYFecha = "";
+
+        // Determina el valor de la variable anterior, en función de si se
+        // trata de un mensaje del propio usuario o uno que ha recibido de
+        // otro usuario.
+        if ($mensaje->getIdUsuarioEmisor() == $_SESSION['id']) {
+            $claseParrafoHoraYFecha = "hora-fecha-mensaje-propio";
+        }
+
+        else {
+            $claseParrafoHoraYFecha = "hora-fecha-mensaje-otro";
+        }
+
+        // Obtiene la fecha completa del mensaje y lo guarda en una
+        // variable que se parseará para obtener los datos horarios
+        // por separado.
+        $datetimeAParsear = date_parse($mensaje->getFecha());
+
+        // Crea un párrafo con la información temporal del mensaje
+        // formateada. En la hora y fecha, se utiliza la función
+        // sprintf, para añadir ceros a la izquierda en aquellos casos
+        // en que el valor en sí no tenga dos cifras.
+        $parrafoHoraYFecha = 
+            '<p class=' . $claseParrafoHoraYFecha . '>' . 
+                sprintf('%02d', $datetimeAParsear['hour']) . ':' . 
+                sprintf('%02d', $datetimeAParsear['minute']) . '   ' . 
+                sprintf('%02d', $datetimeAParsear['day']) . '/' . 
+                sprintf('%02d', $datetimeAParsear['month']) . 
+            '</p>';
+
+        return $parrafoHoraYFecha;
+
+    }
 
 
 
